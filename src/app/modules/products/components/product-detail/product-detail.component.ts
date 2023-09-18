4b6c33cbb0f614972d3d10df92ff987588fb2145
tlkhanh <longkhanhta@gmail.com>
2023-09-19 00:23:58 +0700

    feat: add detail route

--- src/app/modules/products/components/product-detail/product-detail.component.ts
+++ src/app/modules/products/components/product-detail/product-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { ProductsFacade } from './../../product.facade';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,5 +12,9 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent {
-
+  private router=inject(ActivatedRoute);
+  private productsFacade=inject(ProductsFacade);
+  product$=this.router.paramMap.pipe(switchMap(param => {
+    return this.productsFacade.getProductById(Number(param.get('id')))
+  }))
 }
--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ProductsFacade } from './../../product.facade';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
